fc885ae61ad4995410af09f3ffa48361be78615d
Anjar Setyo <setyo.anjar@gmail.com>
2022-03-13 21:59:49 +0700

    update remix config

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,21 @@
+// /**
+//  * @type {import('@remix-run/dev').AppConfig}
+//  */
+// module.exports = {
+//   ignoredRouteFiles: [".*"],
+//   // appDirectory: "app",
+//   // assetsBuildDirectory: "public/build",
+//   // serverBuildPath: "build/index.js",
+//   // publicPath: "/build/",
+//   // devServerPort: 8002
+// };
+
 /**
- * @type {import('@remix-run/dev').AppConfig}
+ * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "api/_build",
 };
