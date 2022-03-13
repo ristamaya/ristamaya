888550c0a962d8c6c0f8c50ca31f5fccc244b496
Anjar Setyo <setyo.anjar@gmail.com>
2022-03-14 04:50:06 +0700

    add component

---
+++ app/routes/shared/components/input.tsx
@@ -0,0 +1,11 @@
+import WorkOnIt from "~/application/components/workonit";
+
+function Input() {
+  return (
+    <div className="w-full">
+      <WorkOnIt />
+    </div>
+  );
+}
+
+export default Input;
---
+++ app/routes/shared/components/table.tsx
@@ -0,0 +1,11 @@
+import WorkOnIt from "~/application/components/workonit";
+
+function Table() {
+  return (
+    <div className="w-full">
+      <WorkOnIt />
+    </div>
+  );
+}
+
+export default Table;
